34a2ef944e6204ca4e4b65b7c4eff682caa3ffa8
mate444 <mateoeo23@gmail.com>
2023-12-10 14:38:58 +0100

    Integrates scripts to MainWindow

--- snake-game/MainWindow.xaml.cs
+++ snake-game/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using snake_game.Enums;
+using snake_game.Scripts;
 
 namespace snake_game
 {
@@ -20,9 +15,176 @@ namespace snake_game
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Dictionary<GridValue, ImageSource> gridValueToImage = new()
+        {
+            { GridValue.Empty, Images.Empty },
+            { GridValue.Snake, Images.Body },
+            { GridValue.Food, Images.Food }
+        };
+
+        private readonly Dictionary<Direction, int> directionToRotation = new()
+        {
+            { Direction.Up, 0 },
+            { Direction.Right, 90 },
+            { Direction.Down, 180 },
+            { Direction.Left, 270 }
+        };
+
+        private readonly int rows = 15, columns = 15;
+        private readonly Image[,] gridImages;
+        private GameState gameState;
+        private bool gameRunning;
+
         public MainWindow()
         {
             InitializeComponent();
+            gridImages = SetupGrid();
+            gameState = new GameState(rows, columns);
+        }
+
+        private async Task RunGame()
+        {
+            Draw();
+            await ShowCountDown();
+            Overlay.Visibility = Visibility.Hidden;
+            await GameLoop();
+            await ShowGameOver();
+            gameState = new GameState(rows, columns);
+        }
+
+        private async void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (Overlay.Visibility == Visibility.Visible)
+            {
+                e.Handled = true;
+            }
+
+            if (!gameRunning)
+            {
+                gameRunning = true;
+                await RunGame();
+                gameRunning = false;
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (gameState.GameOver)
+            {
+                return;
+            }
+
+            switch (e.Key)
+            {
+                case Key.A:
+                    gameState.ChangeDirection(Direction.Left);
+                    break;
+                case Key.D:
+                    gameState.ChangeDirection(Direction.Right);
+                    break;
+                case Key.W:
+                    gameState.ChangeDirection(Direction.Up);
+                    break;
+                case Key.S:
+                    gameState.ChangeDirection(Direction.Down);
+                    break;
+            }
+        }
+
+
+
+        private async Task GameLoop()
+        {
+            while (!gameState.GameOver)
+            {
+                await Task.Delay(150);
+                gameState.Move();
+                Draw();
+            }
+        }
+
+        private Image[,] SetupGrid()
+        {
+            Image[,] images = new Image[rows, columns];
+            GameGrid.Rows = rows;
+            GameGrid.Columns = columns;
+            GameGrid.Width = GameGrid.Height * (columns / (double)rows);
+
+            for (int row = 0; row < rows; row += 1) 
+            {
+                for (int column = 0; column < columns; column += 1)
+                {
+                    Image image = new Image
+                    {
+                        Source = Images.Empty,
+                        RenderTransformOrigin =  new Point(0.5, 0.5)
+                    };
+
+                    images[row, column] = image;
+                    GameGrid.Children.Add(image);
+                }
+            }
+            return images;
+        }
+
+        private void Draw()
+        {
+            DrawGrid();
+            DrawSnakeHead();
+            ScoreText.Text = $"SCORE {gameState.Score}";
+        }
+
+        private void DrawGrid()
+        {
+            for (int row = 0; row < rows; row += 1)
+            {
+                for (int column = 0; column < columns ; column += 1)
+                {
+                    GridValue gridValue = gameState.Grid[row, column];
+                    gridImages[row, column].Source = gridValueToImage[gridValue];
+                    gridImages[row, column].RenderTransform = Transform.Identity;
+                }
+            }
+        }
+
+        private void DrawSnakeHead()
+        {
+            Position headPosition = gameState.HeadPosition();
+            Image image = gridImages[headPosition.Row, headPosition.Column];
+            image.Source = Images.Head;
+
+            int rotation = directionToRotation[gameState.Direction];
+            image.RenderTransform = new RotateTransform(rotation);
+        }
+
+        private async Task DrawDeadSnake()
+        {
+            List<Position> positions = new List<Position>(gameState.SnakePositions());
+
+            for (int i = 0; i < positions.Count; i += 1)
+            {
+                Position position = positions[i];
+                ImageSource source = (i == 0) ? Images.DeadHead : Images.DeadBody;
+                gridImages[position.Row, position.Column].Source = source;
+                await Task.Delay(50);
+            }
+        }
+
+        private async Task ShowCountDown()
+        {
+            for (int i = 3; i >= 1; i -= 1)
+            {
+                OverlayText.Text = i.ToString();
+                await Task.Delay(1000);
+            }
+        }
+
+        private async Task ShowGameOver()
+        {
+            await DrawDeadSnake();
+            await Task.Delay(1000);
+            Overlay.Visibility = Visibility.Visible;
+            OverlayText.Text = "PRESS ANY KEY TO START";
         }
     }
 }
